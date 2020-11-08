5160382be69b7220e26dde8cb198cea2e4b54147
Daniel Schlifka <remingu@techturn.de>
2020-11-08 16:58:04 +0100

    added test skeleton

---
+++ ippool_test.go
@@ -0,0 +1,31 @@
+package ippool
+
+import "testing"
+
+func TestInitPrefixPool(t *testing.T) {
+
+}
+
+func TestRegisterPrefix4(t *testing.T) {
+
+}
+
+func TestRegisterPrefix6(t *testing.T) {
+
+}
+
+func TestRequestIP4(t *testing.T) {
+
+}
+
+func TestRequestIP6(t *testing.T) {
+
+}
+
+func TestReleaseIP4(t *testing.T) {
+
+}
+
+func TestReleaseIP6(t *testing.T) {
+
+}
