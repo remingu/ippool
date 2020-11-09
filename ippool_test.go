36b538c92844a909b1a890e17e3460dbdb05bc3b
Daniel Schlifka <remingu@techturn.de>
2020-11-09 15:21:00 +0100

    added some unittests

--- ippool_test.go
+++ ippool_test.go
@@ -1,13 +1,10 @@
 package ippool
 
 import (
+	"bytes"
 	"net"
 	"testing"
 )
-
-func TestInitPrefixPool(t *testing.T) {
-
-}
 
 func TestRegisterPrefix4(t *testing.T) {
 	// register ip4 prefix
@@ -22,9 +19,9 @@ func TestRegisterPrefix4(t *testing.T) {
 func TestRegisterPrefix4_DuplicatePrefixes(t *testing.T) {
 	// check for duplicate ipv4 prefixes
 	pool := InitPrefixPool()
-	_, IPNet, _ := net.ParseCIDR("192.168.10.96/24")
+	_, IPNet, _ := net.ParseCIDR("172.16.10.96/26")
 	err := RegisterPrefix(&pool, IPNet)
-	_, IPNet, _ = net.ParseCIDR("192.168.10.96/24")
+	_, IPNet, _ = net.ParseCIDR("172.16.10.96/26")
 	err = RegisterPrefix(&pool, IPNet)
 	if err == nil {
 		t.Error()
@@ -53,12 +50,27 @@ func TestRegisterPrefix_DuplicatePrefixes(t *testing.T) {
 	}
 }
 
-func TestRequestIP4(t *testing.T) {
-
+func TestRequestIP4_1(t *testing.T) {
+	// test first assignment
+	expected_addr, _, _ := net.ParseCIDR("192.168.0.1/24")
+	pool := InitPrefixPool()
+	_, IPNet, _ := net.ParseCIDR("192.168.0.0/24")
+	RegisterPrefix(&pool, IPNet)
+	ipaddr, _ := RequestIP(&pool, IPNet)
+	if bytes.Compare(ipaddr, expected_addr[12:16]) != 0 {
+		t.Error()
+	}
 }
 
 func TestRequestIP6(t *testing.T) {
-
+	expected_addr, _, _ := net.ParseCIDR("FE80::1/72")
+	pool := InitPrefixPool()
+	_, IPNet, _ := net.ParseCIDR("FE80::0/72")
+	RegisterPrefix(&pool, IPNet)
+	ipaddr, _ := RequestIP(&pool, IPNet)
+	if bytes.Compare(ipaddr, expected_addr) != 0 {
+		t.Error()
+	}
 }
 
 func TestReleaseIP4(t *testing.T) {
